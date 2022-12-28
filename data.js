9af9d658271b73861dd94b22323836c544a4940c
Itsayu <ayushkr9976@gmail.com>
2022-12-28 17:40:49 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,37 @@
+let movies = [
+    {
+        name: 'Fall',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'images/slider 2.PNG'
+    },
+    {
+        name: 'Avatar',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'images/slider 1.PNG'
+    },
+    {
+        name: 'wanda vision',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'images/slider 3.PNG'
+    },
+    {
+        name: 'Raya',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'images/slider 4.PNG'
+    },
+    {
+        name: 'Luca',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'images/slider 5.PNG'
+    },
+    {
+        name: 'The Falcon',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'images/slider 6.PNG'
+    },
+    {
+        name: 'Ye Rishta Kya Khelata Hai',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'images/slider 7.PNG'
+    },
+]
